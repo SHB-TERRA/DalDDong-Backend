7b05fdff0c9e41bfad09464da665293e228054e6
lbhsos <lbhsos29@naver.com>
2020-11-24 00:30:21 +0900

    [feat] getPromiseLists

--- controllers/promiseController.js
+++ controllers/promiseController.js
@@ -1,10 +1,30 @@
 //import { getPromiseLists, makePromise, deletePromise, getPromiseDetail, joinPromise } from "../controllers/promiseController";
+const { Promise, Sequelize: { Op } } = require('../models');
 
-const getConnection = require('../db.js');
+export const getPromiseLists = async (req, res) => {
+    let result = '';
+    try{
+        result = await Promise.findAndCountAll();
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send(error);
+    }
+    return res.status(200).json(result);
+}
 
-export const getPromiseLists = (req, res) => res.send("getPromiseLists");
-export const makePromise = (req, res) => res.send("makePromise");
-export const deletePromise = (req, res) => res.send("deletePromise");
-export const getPromiseDetail = (req, res) => res.send("getPromiseDetail");
-export const joinPromise = (req, res) => res.send("joinPromise");
+export const makePromise = (req, res) => {
+    res.send("makePromise");
+}
+
+export const deletePromise = (req, res) => {
+    res.send("deletePromise");
+}
+
+export const getPromiseDetail = (req, res) => {
+    res.send("getPromiseDetail");
+}
+
+export const joinPromise = (req, res) => {
+    res.send("joinPromise");
+}
 
