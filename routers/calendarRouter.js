a6dde8f8f5a700dc8cff6f3d20be3d4340e9c8b2
lbhsos <lbhsos29@naver.com>
2020-12-06 15:35:48 +0900

    [fix] login

--- routers/calendarRouter.js
+++ routers/calendarRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
 import routes from "../routes";
 import { getMyCalendar, addPromise, deleteMyPromise } from "../controllers/calendarController";
+import { isLogin, isLogOut } from "../middlewares";
 
 const calendarRouter = express.Router();
 
-calendarRouter.get(routes.userDetail(), getMyCalendar);
-calendarRouter.post(routes.userDetail(), addPromise);
-calendarRouter.delete(routes.promiseDetail(),deleteMyPromise);
+calendarRouter.get(routes.userDetail(), isLogin, getMyCalendar);
+calendarRouter.post(routes.userDetail(), isLogin, addPromise);
+calendarRouter.delete(routes.promiseDetail(), isLogin, deleteMyPromise);
 
 export default calendarRouter;
--- routers/promiseRouter.js
+++ routers/promiseRouter.js
@@ -1,14 +1,15 @@
 import express from "express";
 import routes from "../routes";
 import { getPromiseLists, makePromise, deletePromise, getPromiseDetail, joinPromise } from "../controllers/promiseController";
+import { isLogin, isLogOut } from "../middlewares";
 
 const promiseRouter = express.Router();
 
-promiseRouter.get(routes.home, getPromiseLists);
-promiseRouter.post(routes.home, makePromise);
+promiseRouter.get(routes.home, isLogin, getPromiseLists);
+promiseRouter.post(routes.home, isLogin, makePromise);
 
-promiseRouter.delete(routes.promiseDetail(), deletePromise);
-promiseRouter.get(routes.promiseDetail(), getPromiseDetail);
-promiseRouter.post(routes.promiseDetail(), joinPromise);
+promiseRouter.delete(routes.promiseDetail(), isLogin, deletePromise);
+promiseRouter.get(routes.promiseDetail(), isLogin, getPromiseDetail);
+promiseRouter.post(routes.promiseDetail(), isLogin, joinPromise);
 
 export default promiseRouter;
